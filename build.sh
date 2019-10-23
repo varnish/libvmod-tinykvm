8e7476d4d2c7ece302203d890f5c3a638d72ce4c
Alf-André Walla <fwsgonzo@hotmail.com>
2019-10-23 13:28:39 +0200

    Add build system for varnishtest, shared

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+pushd cmake
 mkdir -p build
 pushd build
 cmake ..
 make -j8
+popd
 popd
--- cmake/varnishtest.cmake
+++
@@ -1,5 +0,0 @@
-cmake_minimum_required (VERSION 3.0.2)
-project(varnishtest C)
-
-set(CMAKE_C_FLAGS "-Wall -Wextra -std=c11 -g -O2")
-option(VARNISH_PLUS "Build with the varnish plus repo" ON)
