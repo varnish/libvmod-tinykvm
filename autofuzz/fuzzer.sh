e1c3428da7abe0f5830e222a09e0ea37e3282ab2
Alf-André Walla <fwsgonzo@hotmail.com>
2019-11-27 15:42:41 +0100

    Initial auto-fuzzing script

---
+++ autofuzz/fuzzer.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# 1. stop old fuzzer
+# 2. git fetch changes
+# 3. checkout latest branch
+# 4. ninja build
+# 5. start new fuzzer
+VCP="ON"
+BUILD_FOLDER="./build"
+CMAKE_FOLDER="$HOME/github/varnish_autoperf/cmake"
+GIT_BRANCH="varnish/6.0-plus"
+
+set -x
+set -e
+
+function stop_fuzzer
+{
+	# might kill someone else?
+	if pgrep "varnishd"; then pkill "varnishd"; fi
+	return $?
+}
+
+function start_fuzzer
+{
+	LD_LIBRARY_PATH=$HOME/llvm/install/lib $BUILD_FOLDER/varnishd
+	return $?
+}
+
+function update_repository
+{
+	pushd ../ext/varnish-cache-plus
+	git checkout $GIT_BRANCH -b fuzzy
+	git pull --rebase=true
+	popd
+}
+
+function build_fuzzer
+{
+	local OPTIONS="-DSINGLE_PROCESS=ON -DVARNISH_PLUS=$VCP -DLIBFUZZER=ON -DSANITIZE=ON"
+	mkdir -p $BUILD_FOLDER
+	pushd $BUILD_FOLDER
+	# Note: Remove -G Ninja to use regular make
+	cmake $CMAKE_FOLDER -G Ninja $OPTIONS
+	ninja
+	popd
+}
+
+echo ">>> Stopping fuzzer..."
+stop_fuzzer
+echo ">>> Retrieving repository changes..."
+update_repository
+echo ">>> Building new fuzzer..."
+build_fuzzer
+echo ">>> Starting fuzzer..."
+start_fuzzer
--- fuzzer.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/env sh
-LD_LIBRARY_PATH=$HOME/llvm/install/lib ./varnishd
