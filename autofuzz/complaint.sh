0e7979cb7972019cfdb7fc6a377403b5ea22e450
Alf-André Walla <fwsgonzo@hotmail.com>
2019-12-05 12:57:11 +0100

    Use filebin for gathering fuzzer crash logs, dumps etc.

--- autofuzz/complaint.sh
+++ autofuzz/complaint.sh
@@ -1,21 +1,51 @@
 #!/bin/bash
 logger "Creating a fuzzer complaint package"
 EMAIL=alf@varnish-software.com
+FILEBIN="https://filebin.varnish-software.com"
+# Generate BIN postfix
+ID="$(hostname)-$(date +'%Y-%m-%d')"
+FILENAME="autofuzz.${ID}.tar.gz"
 
-cd /home/alf/git/varnish_autoperf/autofuzz
+#cd /home/alf/git/varnish_autoperf/autofuzz
 
-# 1. create directory for error report
-mkdir -p files
-rm files/* || true
-mv fuzz-*.log files/ || true
-mv crash-*    files/ || true
-sudo journalctl -xb -1 > files/journal.txt
-coredumpctl dump --output files/core.dump > files/coredump.txt || true
-cp build/varnishd files/
-# 2. mail body
-echo "The fuzzer has stopped" > /tmp/mailbody.txt
-# 3. archive crashes and logs
-tar -czvf logs.tar.gz files/
-# 4. send mail to tech@varnish-software.com
+# Gather logs, binaries and core dump
+fuzzer_gather()
+{
+	mkdir -p files
+	rm files/* || true
+	mv fuzz-*.log files/ || true
+	mv crash-*    files/ || true
+	sudo journalctl -xb -1 > files/journal.txt
+	coredumpctl dump --output files/core.dump > files/coredump.txt || true
+	cp build/varnishd files/
+}
+
+# Upload fuzzer logs to filebin using curl
+filebin_upload()
+{
+	BIN=$(head /dev/urandom | tr -dc a-z0-9 | head -c16)
+	CURLSTATUS=$(curl --data-binary "@$FILENAME" $FILEBIN \
+		-H "bin: ${BIN}" -H "filename: $FILENAME" \
+		--progress-bar --silent --output /dev/null \
+		--connect-timeout 60 --max-time 1800 \
+		--write-out '%{http_code}')
+	if [ ! "$CURLSTATUS" -eq "201" ]
+	then
+		echo "Failed to upload $TGZ to $FILEBIN, http status code: $CURLSTATUS"
+		return 1
+	fi
+	echo "==============================================================================="
+	echo "autofuzz archive: $FILENAME"
+	echo "Uploaded to: $FILEBIN/$BIN"
+	echo "==============================================================================="
+}
+
+# 1. gather logs, binaries, core dump
+fuzzer_gather
+# 2. create and upload archive
+tar -czvf $FILENAME files/
+filebin_upload
+# 3. send mail to tech@varnish-software.com
 logger "Sending mail to $EMAIL"
-cat /tmp/mailbody.txt | sudo mail -s "Fuzzer stopped" -a logs.tar.gz $EMAIL
+echo "The fuzzer has stopped. See the filebin logs here: $FILEBIN/$BIN" > /tmp/mailbody.txt
+cat /tmp/mailbody.txt | sudo mail -s "Fuzzer stopped" $EMAIL
