8794929dd6e627648582f2f65a42658331bf2199
Alf-André Walla <fwsgonzo@hotmail.com>
2020-02-26 12:33:14 +0100

    Resize the banlist too in MSE testing

--- mse/do_test_mse.sh
+++ mse/do_test_mse.sh
@@ -4,6 +4,8 @@ pushd ../build
 make -j4
 
 ./mkfs.mse -f -c ../mse/small.conf
-#gdb --args ./mkfs.mse -r -c ../mse/resized.conf
 ./mkfs.mse -r -c ../mse/resized.conf
+
+echo "*** Just running a check now ***"
+./mkfs.mse --check -c ../mse/small.conf
 popd
