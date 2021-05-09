d1a173d3bad73edfca58f4d2d225060867527434
Alf-André Walla <fwsgonzo@hotmail.com>
2021-05-09 17:13:03 +0200

    Make runrv use optimized code again

--- scripts/runrv.sh
+++ scripts/runrv.sh
@@ -6,5 +6,5 @@ BWS="workspace_backend=1500k"
 mkdir -p /tmp/varnishd
 
 #source $file --vcp=build --single-process --debug --static-sandbox --build $@ --run -a :8080 -f vcl/tenancy.vcl -F -p $BWS
-#source $file --vcp=build --optimize --static-sandbox --build $@ --run -a :8080 -f vcl/tenancy.vcl -F -p $BWS -N
-source $file --vcp=build --no-optimize --static-sandbox --build $@ --run -a :8080 -f vcl/tenancy.vcl -F -p $BWS -N
+source $file --vcp=build --optimize --static-sandbox --build $@ --run -a :8080 -f vcl/tenancy.vcl -F -p $BWS -N
+#source $file --vcp=build --no-optimize --static-sandbox --build $@ --run -a :8080 -f vcl/tenancy.vcl -F -p $BWS -N
