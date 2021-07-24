d29e7fb24b04bd5a1ba5ab97542c75af4886a342
Alf-André Walla <fwsgonzo@hotmail.com>
2021-07-24 19:59:55 +0200

    Update the setup script

--- scripts/vcp_riscv_setup.sh
+++ scripts/vcp_riscv_setup.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 set -e
+# RISC-V sandbox
 pushd vmods/riscv
 git submodule update --init --depth 1 libriscv
 pushd libriscv
 NO_BUILD=1 ./setup.sh
 popd
 popd
+# KVM sandbox
+pushd vmods/kvm
+git submodule update --init --depth 1 tinykvm
+popd
+# Varnish
 pushd ext
 git submodule update --init --depth 1 openssl-cmake
 git submodule update --init --depth 1 varnish-cache-plus
