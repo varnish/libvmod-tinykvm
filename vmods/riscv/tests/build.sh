10979b4b442fbb391d96a2d97b300d6c97843e3c
Alf-André Walla <fwsgonzo@hotmail.com>
2021-08-08 14:50:02 +0200

    vmod_riscv: Update build script and Goto initialization

--- vmods/riscv/tests/build.sh
+++ vmods/riscv/tests/build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
-clang-10 -O0 -Wall -Wextra -I$2 -target riscv32 -march=rv32imfd -ffreestanding -nostdlib -c $1 -o $3.o
-ld.lld-10 -Ttext=0x120000 $3.o -o $3
+if ! command -v "clang-11" &> /dev/null; then
+	export RCC="clang-10"
+	export RLD="ld.lld-10"
+else
+	export RCC="clang-11"
+	export RLD="ld.lld-11"
+fi
+$RCC -O0 -Wall -Wextra -I$2 -target riscv32 -march=rv32imfd -ffreestanding -nostdlib -c $1 -o $3.o
+$RLD -Ttext=0x120000 $3.o -o $3
