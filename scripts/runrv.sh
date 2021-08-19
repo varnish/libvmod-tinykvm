bae51d653d9ec96e741c16c91c04f667b1b35ea4
Alf-André Walla <fwsgonzo@hotmail.com>
2021-08-19 18:29:48 +0200

    Rename tenancy.vcl to riscv.vcl

--- scripts/runrv.sh
+++ scripts/runrv.sh
@@ -5,6 +5,6 @@ file=$dir/cmaketool.sh
 BWS="workspace_backend=64k"
 mkdir -p /tmp/varnishd
 
-#source $file --vcp=build --single-process --debug --static-riscv --build $@ --run -a :8080 -f vcl/tenancy.vcl -F -p $BWS
-#source $file --vcp=build --optimize --static-riscv --build $@ --run -a :8080 -f vcl/tenancy.vcl -F -p $BWS
-source $file --vcp=build --no-optimize --static-riscv --build $@ --run -a :8080 -f vcl/tenancy.vcl -F -p $BWS
+#source $file --vcp=build --single-process --debug --static-riscv --build $@ --run -a :8080 -f vcl/riscv.vcl -F -p $BWS
+#source $file --vcp=build --optimize --static-riscv --build $@ --run -a :8080 -f vcl/riscv.vcl -F -p $BWS
+source $file --vcp=build --no-optimize --static-riscv --build $@ --run -a :8080 -f vcl/riscv.vcl -F -p $BWS
