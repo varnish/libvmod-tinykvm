ae1dba2f3169e3b9b4f2d736ce4fb75f5d2cb73c
Alf-André Walla <fwsgonzo@hotmail.com>
2021-08-24 11:12:28 +0200

    vmod_kvm: Add -n folder to startup script

--- scripts/kvm.sh
+++ scripts/kvm.sh
@@ -3,8 +3,10 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 file=$dir/cmaketool.sh
 
 BWS="workspace_backend=64k"
-mkdir -p /tmp/varnishd
+NTMP="/tmp/varnishd"
+mkdir -p $NTMP
+# fixes VMOD cache bugs
 rm -rf /tmp/varnishd/*
 
-#source $file --vcp=build --optimize --shared-kvm --build $@ --run -a :8080 -f vcl/kvm.vcl -F -p $BWS
-source $file --vcp=build --no-optimize --static-kvm --build $@ --run -a :8080 -f vcl/kvm.vcl -F -p $BWS
+#source $file --vcp=build --optimize --shared-kvm --build $@ --run -a :8080 -f vcl/kvm.vcl -F -n $NTMP -p $BWS
+source $file --vcp=build --no-optimize --static-kvm --build $@ --run -a :8080 -f vcl/kvm.vcl -F -n $NTMP -p $BWS
