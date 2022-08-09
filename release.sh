77243a6b08de1912da775c9a335cac0aea91fbb1
Alf-André Walla <fwsgonzo@hotmail.com>
2022-08-09 10:36:26 +0200

    kvm: Improve folder naming in release script

--- release.sh
+++ release.sh
@@ -1,5 +1,11 @@
 # cmake .. -DKVM_ADNS=ON -DOVERRIDE_VMOD_API="Varnish Plus 6.0.10r1 46f744d2ab5e7c30737dfcf8bbe823b01341efe8"
 
+# python3
+# libbrotli-dev libpcre3-dev
+# 
+#
+
+
 pushd build_alpine
 distrobox enter alpine-latest -- bash -c "\"ninja vmod_kvm\""
 popd
@@ -23,10 +29,10 @@ distrobox enter ubuntu -- bash -c "\"ninja vmod_kvm\""
 popd
 
 cp build_alpine/libvmod_kvm.so ~/vcp6-kvm/vmod/alpine
-cp build_debian10/libvmod_kvm.so ~/vcp6-kvm/vmod/debian10
-cp build_debian11/libvmod_kvm.so ~/vcp6-kvm/vmod/debian11
-cp build_fedora/libvmod_kvm.so ~/vcp6-kvm/vmod/fedora36
-cp build_rlinux8/libvmod_kvm.so ~/vcp6-kvm/vmod/rockylinux8
-cp build_rlinux9/libvmod_kvm.so ~/vcp6-kvm/vmod/rockylinux9
-cp build/libvmod_kvm.so ~/vcp6-kvm/vmod/ubuntu20
-cp build_ubuntu22/libvmod_kvm.so ~/vcp6-kvm/vmod/ubuntu22
+cp build_debian10/libvmod_kvm.so ~/vcp6-kvm/vmod/debian-10
+cp build_debian11/libvmod_kvm.so ~/vcp6-kvm/vmod/debian-11
+cp build_fedora/libvmod_kvm.so ~/vcp6-kvm/vmod/fedora-36
+cp build_rlinux8/libvmod_kvm.so ~/vcp6-kvm/vmod/rocky-8
+cp build_rlinux9/libvmod_kvm.so ~/vcp6-kvm/vmod/rocky-9
+cp build/libvmod_kvm.so ~/vcp6-kvm/vmod/ubuntu-20.04
+cp build_ubuntu22/libvmod_kvm.so ~/vcp6-kvm/vmod/ubuntu-22.04
