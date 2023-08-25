c253881d5275e095250725fdd97cd451dbb573b0
Alf-André Walla <fwsgonzo@hotmail.com>
2023-08-25 22:17:04 +0200

    insular: Remove VMOD

--- vmods/insular/src/vmod_insular.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-#include "../../kvm/src/vmod_kvm.h"
-
-extern int insular_initial_program(VRT_CTX, VCL_PRIV, const char *uri);
-extern int insular_execute(VRT_CTX, int entry, const char *arg);
