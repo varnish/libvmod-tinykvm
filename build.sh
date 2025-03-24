639ee0684b48126aad96f3bd86e5b3b2cae43dc3
Alf-André Walla <fwsgonzo@hotmail.com>
2025-03-24 18:07:57 +0100

    Add some build options and disable jemalloc for VE

--- build.sh
+++ build.sh
@@ -1,11 +1,50 @@
 #!/bin/bash
 set -e
 
+#!/bin/bash
+set -e
+export CC="ccache $CC"
+export CXX="ccache $CXX"
+cmake_extra=""
+varnish_plus="OFF"
+
+usage() {
+	echo "Usage: $0 [-v] [--enterprise]"
+	echo "  -v            verbose build"
+	echo "  --enterprise  build for Varnish Enterprise"
+	exit 1
+}
+
+for i in "$@"; do
+	case $i in
+		--enterprise)
+            varnish_plus="ON"
+            shift
+            ;;
+		-v)
+			export VERBOSE=1
+			shift
+			;;
+		-*|--*)
+			echo "Unknown option $i"
+			exit 1
+			;;
+		*)
+		;;
+	esac
+done
+
 mkdir -p .build
 pushd .build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DVARNISH_PLUS=OFF
+cmake .. -DCMAKE_BUILD_TYPE=Release -DVARNISH_PLUS=$varnish_plus $cmake_extra
 cmake --build . -j6
 popd
 
-echo "Installing vmod into /usr/lib/varnish/vmods/"
-sudo cp .build/libvmod_*.so /usr/lib/varnish/vmods/
+VPATH="/usr/lib/varnish/vmods/"
+VEPATH="/usr/lib/varnish-plus/vmods/"
+if [ "$varnish_plus" == "ON" ]; then
+	VPATH=$VEPATH
+fi
+
+echo "Installing vmod into $VPATH"
+sudo cp .build/libvmod_*.so $VPATH
